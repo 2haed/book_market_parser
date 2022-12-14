6584c7380f436655d5d81499b8f1286758fffadb
2haed <yurlov.daniil1@yandex.ru>
2022-12-14 14:09:28 +0300

    from pc

---
+++ async_parser.py
@@ -0,0 +1,46 @@
+import json
+import logging
+import warnings
+
+import aiohttp
+import requests
+
+from data.constants import HEADERS
+
+
+warnings.filterwarnings("ignore", category=DeprecationWarning)
+logging.basicConfig(filename='data/errors.log', format='%(asctime)s| %(message)s', datefmt='%m-%d-%Y %I:%M:%S',
+                    level=logging.INFO)
+
+
+dictionary = dict(items=[])
+for offset in range(0, 500, 100):
+    url = f"https://api.detmir.ru/v2/products?filter=brands[].id:7;categories[].alias:lego;platform:web;promo:false;site:detmir;withregion:RU-MOW&expand=meta.facet.ages.adults,meta.facet.gender.adults,webp&meta=*&limit=502&offset={offset}&sort=popularity:desc"
+    response = requests.request("GET", url, headers=HEADERS)
+    products = []
+    for items in response.json()['items']:
+        item = {
+            'price': items['price']['price'],
+            'productId': items['productId'],
+            'type': items['type'],
+            'article': items['article'],
+            'title': items['title'],
+            'rating': items['rating'],
+            'link': items['link']['web_url'],
+            'pictures': items['pictures'][0]['original']
+        }
+        products.append(item)
+    dictionary['items'] += products
+
+with open('data/json.json', 'w', encoding='UTF-8') as json_file:
+    json.dump(dictionary, json_file, indent=4, ensure_ascii=False)
+
+async def main():
+    async with aiohttp.ClientSession as session:
+        async with session.get()
+
+# with open('json.json', 'r', encoding='UTF-8') as file:
+#     data = json.load(file)
+#     items = list()
+#     for product in data['items']:
+#         print(product)
